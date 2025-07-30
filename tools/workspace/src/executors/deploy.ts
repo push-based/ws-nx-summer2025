c560b53417d7877068f11d024d34921525d4fd1b
Hyana Kang <hyana.kang@joinhandshake.com>
2025-07-30 12:08:12 -0400

    first day change

---
+++ tools/workspace/src/executors/deploy.ts
@@ -0,0 +1,19 @@
+import { PromiseExecutor } from '@nx/devkit';
+import { DeployExecutorSchema } from './schema';
+import { exec } from 'node:child_process';
+
+const runExecutor: PromiseExecutor<DeployExecutorSchema> = async (
+  options,
+  context,
+) => {
+  const projectName = context.projectName;
+
+  exec(
+    `docker build -f ${options.dockerFile} --build-arg='APP_NAME=${projectName}' . -t ${options.tag}`,
+  );
+  return {
+    success: true,
+  };
+};
+
+export default runExecutor;
