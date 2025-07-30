83f4377e29f3b2ed975edc977003583cc1d305f7
dallas-scott <95313962+dallas-scott@users.noreply.github.com>
2025-07-30 14:02:28 -0500

    Update main.tsx

--- apps/movies/src/main.tsx
+++ apps/movies/src/main.tsx
@@ -12,7 +12,7 @@ root.render(
   <StrictMode>
     <AuthProvider>
       <MovieProvider>
-        <App />
+        <App data-testid="test" />
       </MovieProvider>
     </AuthProvider>
   </StrictMode>
