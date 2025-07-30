070478937146f2e1b23dbd6bd6e43c991d789668
dallas-scott <95313962+dallas-scott@users.noreply.github.com>
2025-07-30 15:44:18 -0500

    flakey test

--- apps/movies-e2e/src/movie-detail.spec.ts
+++ apps/movies-e2e/src/movie-detail.spec.ts
@@ -38,7 +38,7 @@ test('flaky test example', async ({ page }) => {
 
   // Randomly fail this test to demonstrate flaky behavior
   if (Math.random() < 0.5) {
-    throw new Error('This is a flaky test that fails randomly.');
+    throw new Error('This is a flaky test that randomly succeeds.');
   }
 
   // If it doesn't fail, check if the title is displayed correctly
