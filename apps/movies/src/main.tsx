a94ed8eb53c2614d874c2887e47c589d50a43f1b
Nathan Somsa-ard <nathan.somsaard@hcl.com>
2025-07-30 10:24:56 -0700

    chore: Apply exercises implementations

--- apps/movies/src/main.tsx
+++ apps/movies/src/main.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
 import { AuthProvider } from '@react-monorepo/shared-auth-data-access';
 import { MovieProvider } from '@react-monorepo/movies-data-access';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +13,9 @@ root.render(
   <StrictMode>
     <AuthProvider>
       <MovieProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </MovieProvider>
     </AuthProvider>
   </StrictMode>
