baca606e5f05aa17768449f55660a50e96a4b5f0
Tim Capps <tcapps@360view.com>
2025-07-30 14:58:01 -0400

    testing out agents

--- apps/movies/src/app/app.tsx
+++ apps/movies/src/app/app.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
 
 export function App() {
-  // Hello from tcapps (update 1.0)
+  // Hello from tcapps (update 2.0)
   return <RouterProvider router={router} />;
 }
 
