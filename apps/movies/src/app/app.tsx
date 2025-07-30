a02b0b4932979b6733326bdba0f606c1c34b70dc
Julian Jandl <julian@jandl.wien>
2025-07-30 19:43:30 +0200

    bust the cache

--- apps/movies/src/app/app.tsx
+++ apps/movies/src/app/app.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
 
 export function App() {
+  // hey this is a comment
   return <RouterProvider router={router} />;
 }
 
