bf1494d233406b7c94d6e8dcbf837ee8f70c4a89
Ross Blumhorst <rblumhorst@entegral.com>
2025-07-30 12:39:08 -0500

    add comment

--- apps/movies/src/app/app.tsx
+++ apps/movies/src/app/app.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
 
 export function App() {
+  // comment 
   return <RouterProvider router={router} />;
 }
 
