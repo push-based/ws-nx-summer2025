dc1b4fc47eb93505ff2eba60155586883a8cf32b
dallas-scott <95313962+dallas-scott@users.noreply.github.com>
2025-07-30 12:24:55 -0500

    this is everything from me

---
+++ tools/workspace/src/executors/deploy.ts
@@ -0,0 +1,35 @@
+
+// exectuor
+import { logger, PromiseExecutor } from '@nx/devkit';
+
+import { DeployExecutorSchema } from './schema';
+
+import { promisify } from 'node:util';
+import { exec } from 'node:child_process';
+
+const asyncExec = promisify(exec);
+
+const runExecutor: PromiseExecutor<DeployExecutorSchema> = async (
+  options,
+  context
+) => {
+  const projectName = context.projectName;
+  logger.log({ projectName, options });
+  try {
+    const { stderr } = await asyncExec(
+      `docker build -f ${options.dockerfile} --build-arg='APP_NAME=${projectName}' . -t ${options.tag}`
+    );
+    logger.log(stderr);
+    logger.log('thank you for deploying with austrian airlines');
+    return {
+      success: true,
+    };
+  } catch (e) {
+    logger.error(e.message);
+    return {
+      success: false,
+    };
+  }
+};
+
+export default runExecutor;
