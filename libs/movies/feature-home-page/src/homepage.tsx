e07b90b11a3477a17c512fdc0f7693cb50cd0c2d
Sterling McCall <smccall@entegral.com>
2025-07-30 13:18:51 -0500

    exercises 1-14

--- libs/movies/feature-home-page/src/homepage.tsx
+++ libs/movies/feature-home-page/src/homepage.tsx
@@ -5,7 +5,8 @@ import { MovieCard } from '@react-monorepo/movies-ui';
 import { AuthContext } from '@react-monorepo/shared-auth-data-access';
 import { useFavorites, useMovies } from '@react-monorepo/movies-data-access';
 import { useSearchParams } from 'react-router-dom';
-import { searchMoviesByTitle } from '../utils/search-movies-by-title.util';
+import { searchMoviesByTitle } from '@react-monorepo/movies-utils';
+// import { searchMoviesByTitle } from '';
 
 export default function Homepage() {
   const { user } = useContext(AuthContext);
