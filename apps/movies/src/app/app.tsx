30113d1864fbc79a1cdfb9b5638fa5041a20e693
Paul <shadowboxlinux@gmail.com>
2025-07-30 10:38:42 -0700

    update app.tsx

--- apps/movies/src/app/app.tsx
+++ apps/movies/src/app/app.tsx
@@ -2,6 +2,8 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
 
 export function App() {
+  console.log('App component is rendering');
+
   return <RouterProvider router={router} />;
 }
 
