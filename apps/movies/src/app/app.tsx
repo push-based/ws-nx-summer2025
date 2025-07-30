3db7258587307fdc5ae66a74e32f1344eb8d45bc
Travis Baker <baker.travis.w@gmail.com>
2025-07-30 14:49:18 -0600

    fail, dangit!

--- apps/movies/src/app/app.tsx
+++ apps/movies/src/app/app.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
 
 export function App() {
-  console.log('heres a movie 3!');
+  console.log('heres a movie 4!');
   return <RouterProvider router={router} />;
 }
 
