be909ae05498460725c7c9eb455585ec471d80ac
Tim Capps <tcapps@360view.com>
2025-07-30 14:17:16 -0400

    changes, cache warmup, push

--- apps/movies/src/app/app.tsx
+++ apps/movies/src/app/app.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
 
 export function App() {
-  // Hello from tcapps
+  // Hello from tcapps (update 1.0)
   return <RouterProvider router={router} />;
 }
 
